ac50ba158e8a7bbd7d2b73b39afb7ce8458def8f
Marco <mespinoza1986@gmail.com>
2024-08-27 19:21:05 -0600

    updating jornadas

--- js/resultados-totales.js
+++ js/resultados-totales.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {    
+document.addEventListener('DOMContentLoaded', () => {
     const calcularButton = document.querySelector('#calcularResultados');
 
     calcularButton.addEventListener('click', () => {
